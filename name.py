5678307716d52cc66ea69fb8e91e1dba2563dbcb
Adewole-211 <76133232+Adewole-211@users.noreply.github.com>
2022-10-16 19:05:03 +0100

    Add files via upload

---
+++ name.py
@@ -0,0 +1,18 @@
+store_name = "sarah's Store"
+print(store_name)
+
+
+hello = "Hello"
+name = "Sarah"
+greeting = hello + " " + name
+print (greeting)
+
+hello =  "Hello"
+name = input ("What's your name")
+greeting = hello + " " + name
+print (greeting)
+
+hello =  "Hello"
+name = input ("What's your name?\n")
+greeting = hello + " " + name
+print (greeting)
